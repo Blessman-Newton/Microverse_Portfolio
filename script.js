69f321287e84fcdd3ac46b4b1234f15564f61084
Blessman-Newton <blessmannewton0@gmail.com>
2023-05-23 13:40:19 +0100

    modify hamburger menu

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-/* ---------------------hamburger start here! below----------------------------- */
+/*---------------------hamburger start here! below----------------------------- */
 const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.navtabs');
+const Menu = document.querySelector('.navtabs');
 const backMenu = document.querySelectorAll('#list');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  menu.classList.toggle('active');
+  Menu.classList.toggle('active');
 });
 
 backMenu.forEach((element) => element.addEventListener('click', () => {
   hamburger.classList.remove('active');
-  menu.classList.remove('active');
+  Menu.classList.remove('active');
 }));
 
-/* ---------------------hamburger code above----------------------------- */
+/*---------------------hamburger code above----------------------------- */
+
+/*---------------------Windows popup code below----------------------------- */
+const body = document.querySelector('body');
+const view = document.querySelector('.view-project');
+view.addEventListener('click', ()=> {
+  const show = document.createElement('div');
+  const detail_pop = document.createElement('div');
+  show.className = 'show';
+  detail_pop.className = 'pop';
+  detail_pop.innerHTML = `
+                  <div class="popup-header">
+                    <h2>Project Name</h2>
+                    <span id="close">&times;</span>
+                  </div>
+                    <div class="popup-button">
+                      <button>1233</button>
+                      <button>4322333</button>
+                      <button>3333333</button>
+                    </div>
+                  <div class="popup-body">
+                    <img src="images/Snap.png" alt="">
+                      <p>
+                      Lorem Ipsum is simply dummy text of the 
+                      printing and typesetting industry. 
+                      Lorem Ipsum has been the industry's 
+                      standard dummy text ever since the 1500s, 
+                      when an unknown printer took a galley of type and scrambled it 1960s.
+
+                      Lorem Ipsum is simply dummy text of the printing and 
+                      typesetting industry. Lorem Ipsum has been the industry's 
+                      standard dummy text ever since the 1500s, when an unknown 
+                      printer took a galley of type and scrambled it 1960s 
+                      with the releax map lapora verita.
+                      </p>
+                    <div class="popup-button">
+                      <button>444444</button>
+                      <button>444444444</button>
+                    </div>
+                  </div>
+
+  `
+show.appendChild(detail_pop);
+body.appendChild(show);
+
+const close = document.querySelector('#close');
+
+close.addEventListener('click', ()=> {
+  body.removeChild(show);
+});
+});
+
+/*---------------------Windows popup code above----------------------------- */
