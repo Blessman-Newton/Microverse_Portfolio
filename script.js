104dbfeb23a686206d2dc802cf75279e824d6999
Blessman-Newton <blessmannewton0@gmail.com>
2023-05-26 05:43:00 +0100

    modify the script file

--- script.js
+++ script.js
@@ -242,7 +242,7 @@ view.forEach((element) => element.addEventListener('click', () => {
                           <p class="desktop">
                           Lorem Ipsum is simply dummy text of the
                           printing and typesetting industry.
-                          Lorem Ipsum has been the industry's
+                          Lorem Ipshum has been the industry's
                           standard dummy text ever since the 1500s,
                           when an unknown printer took a
                            galley of type and scrambled it 1960s.
